51c3d92bd5c2a47481e549c81d1ef816e0d3209f
Fernando Carelli <88006062+FernandoCarelliNunes@users.noreply.github.com>
2026-03-17 13:59:46 -0300

    Modify settings.py to use dj_database_url
    Updated database configuration to use dj_database_url for production.

--- plataforma_rhino/setup/settings.py
+++ plataforma_rhino/setup/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +81,19 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Substitua pelo link que o Render te der (Internal Database URL)
+        # Se não encontrar a variável, ele usa o sqlite local por segurança
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
