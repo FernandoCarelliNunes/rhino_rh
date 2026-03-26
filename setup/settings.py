204260f301ec0d48142e32a33e51e0f1f10f8af2
Fernando Carelli <88006062+FernandoCarelliNunes@users.noreply.github.com>
2026-03-26 16:09:40 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,7 +136,7 @@ if not DEBUG:
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 
-import os
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
