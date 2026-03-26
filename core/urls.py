4faeb9f2969c0b99cad1889ad884df997a2c1d3a
Fernando Carelli <88006062+FernandoCarelliNunes@users.noreply.github.com>
2026-03-26 17:25:12 -0300

    Add files via upload

---
+++ core/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('candidato/novo/', views.cadastrar_candidato, name='cadastrar_candidato'),
+    path('vaga/<int:vaga_id>/', views.detalhes_vaga, name='detalhes_vaga'),
+    path('candidato/<int:candidato_id>/status/<str:novo_status>/', views.mudar_status_candidato, name='mudar_status'),
+    path('vaga/nova/', views.cadastrar_vaga, name='cadastrar_vaga'),
+    path('vaga/<int:vaga_id>/status/<str:novo_status>/', views.mudar_status_vaga, name='mudar_status_vaga'),
+    path('relatorios/geral/', views.relatorio_geral, name='relatorio_geral'),
+    path('cliente/novo/', views.cadastrar_cliente, name='cadastrar_cliente'),
+    path('vaga/<int:vaga_id>/inscrever/', views.inscrever_vaga, name='inscrever_vaga'),
+    path('candidato/<int:candidato_id>/status/<str:novo_status>/',views.mudar_status_candidato, name='mudar_status_candidato'),
+    path('vagas/exportar-pdf/', views.exportar_vagas_pdf, name='exportar_vagas_pdf'),
+    path('relatorio/', views.relatorio_vagas, name='relatorio_vagas'),
+]
+ 
+
+
