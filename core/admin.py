ef2c9642d9f95b0a621af015216c4299c1351ab3
Fernando Carelli <88006062+FernandoCarelliNunes@users.noreply.github.com>
2026-03-26 17:22:42 -0300

    Delete core directory

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Cliente, Vaga, Candidato, HistoricoStatus
-
-admin.site.register(Cliente)
-admin.site.register(Vaga)
-admin.site.register(Candidato)
-admin.site.register(HistoricoStatus)
